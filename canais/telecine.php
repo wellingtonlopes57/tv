ca5fda2cd89b532b25ffda6009a470c815812278
wellingtonlopes57 <80447410+wellingtonlopes57@users.noreply.github.com>
2021-03-28 00:18:06 -0300

    Add files via upload

---
+++ canais/telecine.php
@@ -0,0 +1,22 @@
+<style>
+#hls-example {
+    width: 100%;
+    height: 100%;
+}
+body {
+    margin: 0!important;
+}
+</style>
+<!-- CSS  -->
+<link href="https://vjs.zencdn.net/7.2.3/video-js.css" rel="stylesheet">
+<!-- HTML -->
+<video id='hls-example'  class="video-js vjs-default-skin" controls>
+<source type="application/x-mpegURL" src="http://iptv.leoreis.me/live/6.m3u8">
+</video>
+<!-- JS code -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/videojs-contrib-hls/5.14.1/videojs-contrib-hls.js"></script>
+<script src="https://vjs.zencdn.net/7.2.3/video.js"></script>
+<script>
+var player = videojs('hls-example');
+player.play();
+</script>
